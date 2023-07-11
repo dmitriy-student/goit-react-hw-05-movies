7e9ca35652e8d7cd090d974b57bfd85a8841a63c
dmitriy-student <dima.ermol22@gmail.com>
2023-07-12 02:06:30 +0300

    add start files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,47 @@
+import { NavLink, Route, Routes } from 'react-router-dom';
+import styled from 'styled-components';
+
+import Home from 'pages/Home';
+import Movies from 'pages/Movies';
+import Cast from './Cast/Cast';
+import Reviews from './Reviews/Reviews';
+import MovieDetails from 'pages/MovieDetails';
+import NotFound from 'pages/NotFound';
+
+const NavLinkStyled = styled(NavLink)`
+  color: black;
+
+  &.active {
+    color: orange;
+  }
+`;
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <header>
+        <nav>
+          <ul>
+            <li>
+              <NavLinkStyled to="/">Home</NavLinkStyled>
+            </li>
+            <li>
+              <NavLinkStyled to="/movies">Movies</NavLinkStyled>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/movies/" element={<Movies />} />
+          <Route path="/movies/:movieId" element={<MovieDetails />}>
+            <Route path="cast" element={<Cast />} />
+            <Route path="reviews" element={<Reviews />} />
+          </Route>
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </main>
+    </>
   );
 };
